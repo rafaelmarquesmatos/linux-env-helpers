a5937395c1f326b65ff57024b15a5398b2fdd867
markum <marquesdematosrafael@gmail.com>
2026-05-13 14:03:43 -0300

    adicionando env base do container arch

---
+++ containers/arch/scripts/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NAME="$1"
+
+if [ -z "$NAME" ]; then
+	echo "uso: ./script.sh <nome do container>"
+	exit 1
+fi
+
+if ! command -v podman >/dev/null 2>&1; then
+	echo "podman não encontrado."
+	exit 1
+fi
+
+podman run -it "$NAME"
